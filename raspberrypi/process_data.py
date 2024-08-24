2ac97c56566961b2225201bdacb96ef3d59b6a61
Kevin Pelgrims <kevin.pelgrims@gmail.com>
2024-08-24 22:16:15 +0200

    Set up scheduler for reading sensor data from RPi
    We want to get the data every 15 minutes. The schedule library helps us prevent drift, because every time we open the serial connection, we need to wait a bit before the data shows up.

--- raspberrypi/process_data.py
+++ raspberrypi/process_data.py
@@ -1,24 +1,39 @@
-from datetime import datetime
-
-import serial
 import json
 import time
+from datetime import datetime
+
+import schedule
+import serial
 
 
-def read_serial_data(serial_connection):
+def read_serial_data(serial_port):
+    # Configure the serial connection
+    # We open and close it every 15 minutes so that we don't block any other processes from accessing the serial port.
+    serial_connection = serial.Serial(f'/dev/{serial_port}', 9600, timeout=1)
+    # Set backoff to 5 seconds, because that's the frequency at which the Arduino is emitting data
+    read_backoff_in_seconds = 5
+
     try:
-        if serial_connection.in_waiting == 0:
-            print(f"{datetime.now()} No data available")
-            return
+        # Read data from the serial port (with retries)
+        for _ in range(3):
+            if serial_connection.in_waiting > 0:
+                # Read data from the serial port
+                data = serial_connection.readline().decode('utf-8').strip()
 
-        # Read data from the serial port
-        data = serial_connection.readline().decode('utf-8').strip()
+                # Parse the JSON data
+                json_data = json.loads(data)
 
-        # Parse the JSON data
-        json_data = json.loads(data)
+                # Print the data to console
+                print(f"{datetime.now()} Received data: {json_data}")
 
-        # Print the data to console
-        print(f"{datetime.now()} Received data: {json_data}")
+                serial_connection.close()
+
+                return
+            else:
+                print(f"{datetime.now()} No data available yet. Will retry in {read_backoff_in_seconds} seconds...")
+                time.sleep(read_backoff_in_seconds)
+
+        print("Error: No data received after 3 attempts")
 
     except json.JSONDecodeError:
         print("Error: Invalid JSON data received")
@@ -27,20 +42,18 @@ def read_serial_data(serial_connection):
 
 
 def main(serial_port='ttyACM0'):
-    print("Starting serial data reader. Press Ctrl+C to exit.")
+    # Read the data every 15 minutes
+    schedule.every(15).minutes.do(read_serial_data, serial_port)
 
-    # Configure the serial connection
-    serial_connection = serial.Serial(f'/dev/{serial_port}', 9600, timeout=1)
-    serial_connection.flush()
+    print(f"Starting serial data reader on port /dev/{serial_port}. Press Ctrl+C to exit.")
 
     try:
+        read_serial_data(serial_port)
         while True:
-            read_serial_data(serial_connection)
-            time.sleep(5)
+            schedule.run_pending()
+            time.sleep(1)
     except KeyboardInterrupt:
         print("\nExiting program.")
-    finally:
-        serial_connection.close()
 
 
 if __name__ == "__main__":
