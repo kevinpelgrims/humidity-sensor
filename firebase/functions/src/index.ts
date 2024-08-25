be323b292cf889a784c7e6263a3fddf9ae1f48d4
Kevin Pelgrims <kevin.pelgrims@gmail.com>
2024-08-25 02:06:05 +0200

    Add Firebase function to fetch weather conditions
    Whenever there is a new row added to the sensor readings, get the current weather conditions and store those to the database.
    
    This way we can correlate sensor data and weather data over time.

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,19 +1,41 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-import {onRequest} from "firebase-functions/v2/https";
+import {onDocumentCreated} from "firebase-functions/v2/firestore";
 import * as logger from "firebase-functions/logger";
+import {initializeApp} from "firebase-admin/app";
+import {getFirestore, Timestamp} from "firebase-admin/firestore";
+import {getRemoteConfig} from "firebase-admin/remote-config";
+import {getPrecipitationSeverity, getWeatherDescription, WeatherApiResponse, WeatherRecord} from "./weather_helper.js";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+initializeApp();
+const database = getFirestore();
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const fetchWeatherConditions = onDocumentCreated(
+  "sensor_readings/{docId}",
+  async () => {
+    try {
+      const latitude = 55.68;
+      const longitude = 12.49;
+
+      // eslint-disable-next-line max-len
+      const weatherApiUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,precipitation,rain,showers,weather_code,wind_speed_10m`;
+
+      const response = await fetch(weatherApiUrl);
+      const weatherData: WeatherApiResponse = await response.json();
+
+      const weatherRecord: WeatherRecord = {
+        time: Timestamp.fromDate(new Date(weatherData.current.time)),
+        temperature: weatherData.current.temperature_2m,
+        precipitation: weatherData.current.precipitation,
+        precipitationSeverity: getPrecipitationSeverity(weatherData.current.precipitation),
+        rain: weatherData.current.rain,
+        showers: weatherData.current.showers,
+        windSpeed: weatherData.current.wind_speed_10m,
+        weatherCode: weatherData.current.weather_code,
+        weatherDescription: getWeatherDescription(weatherData.current.weather_code),
+      };
+
+      await database.collection("weather_records").add(weatherRecord);
+    } catch (error) {
+      logger.error(error);
+    }
+  }
+);
