ed247b884719c53f2b30675621d7f5a9db3cbac5
Kevin Pelgrims <kevin.pelgrims@gmail.com>
2024-08-28 22:00:42 +0200

    Set up Firebase library for dashboard

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,8 +1,17 @@
 import './App.css'
 import SensorData from "@/components/SensorData.tsx";
 import WeatherData from "@/components/WeatherData.tsx";
+import {initializeApp} from "firebase/app";
 
 function App() {
+  const firebaseConfig = {
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    appId: import.meta.env.VITE_APP_ID,
+    apiKey: import.meta.env.VITE_API_KEY,
+  }
+
+  const firebaseApp = initializeApp(firebaseConfig);
+
   return (
     <div className="p-4 space-y-8">
       <h1 className="text-3xl font-bold mb-4">Humidity Dashboard</h1>
